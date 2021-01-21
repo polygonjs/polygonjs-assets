3fad478e9036e99a9f5a28aba5072e9b20499820
Guillaume Fradin <guillaumefradin@gmail.com>
2021-01-21 19:12:31 +0000

    start sample files

---
+++ docs/samples/snowpack/javascript.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "mount": {
+        "./public/tex": {
+            url: '/tex',
+            static: true
+        },
+        "./public/models": {
+            url: '/models',
+            static: true
+        },
+        "./": {
+            url: '/',
+        },
+    }
+};
