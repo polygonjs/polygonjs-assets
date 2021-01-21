75f9d5c289d89a45fe34bb6ebf7c40b5615595ef
Guillaume Fradin <guillaumefradin@gmail.com>
2021-01-21 19:16:41 +0000

    add js installation in one bash script

---
+++ docs/samples/getting_started_javascript.sh
@@ -0,0 +1,3 @@
+npm init -y;
+yarn add polygonjs-editor polygonjs-engine;
+wget https://raw.githubusercontent.com/polygonjs/polygonjs-assets/master/docs/samples/snowpack/javascript.js -O snowpack.config.js;
