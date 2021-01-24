192db18eacab82fd030390b8997b0f302427bb3f
Guillaume Fradin <guillaumefradin@gmail.com>
2021-01-24 17:24:47 +0000

    install script update polygonjs npm module path from polygonjs-engine to @polygonjs/polygonjs

--- docs/samples/getting_started_javascript.sh
+++ docs/samples/getting_started_javascript.sh
@@ -1,3 +1,3 @@
 npm init -y;
-yarn add polygonjs-editor polygonjs-engine;
+yarn add polygonjs-editor @polygonjs/polygonjs;
 wget https://raw.githubusercontent.com/polygonjs/polygonjs-assets/master/docs/samples/snowpack/javascript.js -O snowpack.config.js;
